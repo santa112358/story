82b5549abb9958e723a80857d676699a3571c849
Isaias Mejia <imejiasoft@gmail.com>
2021-03-11 12:07:02 -0400

    Remove Key Of Example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -36,8 +36,6 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
